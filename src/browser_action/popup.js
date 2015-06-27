bd40dbe88709eb44242dbe9c8faea2bfa94b1c70
Yu Chen Hou <houyuchen66@gmail.com>
2015-06-27 07:42:04 -0400

    popup menu

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,7 +1,18 @@
 var killswitch = function(){
-    chrome.runtime.sendMessage({killswitch: true});
+  chrome.runtime.sendMessage({killswitch: true});
+  document.querySelector(".start-button").className = "start-button";
+  document.querySelector(".stop-button").className += " hidden";
 }
 
+var startswitch = function(){
+  chrome.runtime.sendMessage({killswitch: true});
+  document.querySelector(".stop-button").className = "stop-button";
+  document.querySelector(".start-button").className += " hidden";
+}
+
+var settings = function(){
+  chrome.runtime.sendMessage({killswitch: true});
+}
 function main() {
   // Initialization work goes here.
 }
@@ -10,6 +21,8 @@ function main() {
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', killswitch);
+  document.querySelector('.stop-button').addEventListener('click', killswitch);
+  document.querySelector('.start-button').addEventListener('click', startswitch);
+  document.querySelector('.settings-button').addEventListener('click', settings);
   main();
 });
