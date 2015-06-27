9db713b282aabac76aa41e736fe1a5ca747f7888
jgzuke <jgzuke@gmail.com>
2015-06-26 23:32:34 -0400

    gave swipe event

--- doppler/testing.js
+++ doppler/testing.js
@@ -1,23 +1,49 @@
+// CONSTANTS
+var threshold = 4;
+var swipeThreshold = 3;
+var scale    = 10;
+var baseSize = 100;
+
+// TRACKING VARS
+var lastVal = 0;
+
+function swipeEvent(isRight) {
+  if(isRight) {
+    document.getElementById('click').innerHTML = "Right";
+  } else {
+    document.getElementById('click').innerHTML = "Left";
+  }
+}
+
+function stringLen(length) { 
+    var s = ''; 
+    while (s.length < length) s += "**"; 
+    return s; 
+} 
+
 window.addEventListener('load', function() {
   window.doppler.init(function(bandwidth) {
-    var threshold = 4;
     if (bandwidth.left > threshold || bandwidth.right > threshold) {
-        var scale    = 10;
-        var baseSize = 100;
-        var diff = bandwidth.left - bandwidth.right;
+      var diff = bandwidth.left - bandwidth.right;
+
+      if(lastVal != diff) {
+        lastVal = diff;
+
+        console.log(stringLen(diff+10));
+
         var dimension = (baseSize + scale*diff) + 'px';
 
-        console.log(dimension);
         document.getElementById('box').style.width  = dimension;
         document.getElementById('box').style.height = dimension;
 
-        if(dimension < 100) {
-          document.getElementById('click').innerHTML = "Left";
-        } else if(dimension <= 130) {
+        if(diff < -swipeThreshold) {
+          swipeEvent(false);
+        } else if(diff <= swipeThreshold) {
           document.getElementById('click').innerHTML = "None";
         } else {
-          document.getElementById('click').innerHTML = "Right";
+          swipeEvent(true);
         }
       }
-    });
+    }
   });
+});
