453dbdd3c92b492180da97728606b7096147335d
Yu Chen Hou <houyuchen66@gmail.com>
2015-06-26 22:43:31 -0400

    permissions

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,8 +1,8 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+var settings = new Store("settings", {
+     "sample_setting": "This is how you use Store.js to remember values"
+});
 
 
 //example of using a message handler from the inject scripts
@@ -10,4 +10,4 @@ chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
-  });
+});
